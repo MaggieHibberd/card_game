d2873718c1ef10a8eeeae8f97e73b0d6f69e6c17
Maggie Hibberd <maggiehibberd@gmail.com>
2021-07-21 16:09:24 +0100

    First test passing, card deck implemented and method to view ful deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,19 @@
 class Deck 
-FULL_DECK = []
-
-
+  FULL_DECK = []
+def initialize
+  suits = ["spades", "diamonds", "hearts", "clubs"]
+  ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+  suits.each do |suit|
+    ranks.each do |rank|
+        FULL_DECK << rank + " " + suit
+      end
+    end
 end
+
+  def check_deck
+    FULL_DECK.each do |card|
+        card
+    end
+  end
+end
+
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,8 +3,9 @@ require 'deck'
 describe Deck do 
   context 'Class starts with a full deck of cards' do 
     it 'initializes with a deck of cards' do
-    deck = Deck.new 
-    expect(deck.check_deck).to include 'A spades'
+    deck = Deck.new
+    sample = deck.check_deck.sample
+    expect(deck.check_deck).to include sample
   end
 end
 end
